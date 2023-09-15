4cdf35877f0192a6bb5a8b6ed5b29b78d2de14a9
KimJJRoSY <kjjeaus@gmail.com>
2023-09-15 10:36:34 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+import styles from "@/styles/home.module.css";
+
+export default function Home() {
+  return <h1 className={styles.title}>안녕안녕</h1>;
+}
---
+++ pages/products/[id].js
@@ -0,0 +1,9 @@
+export default function Product() {
+  return <div>Product</div>;
+}
+
+//function이름보다는 파일 이름으로 웹 페이지 생성됨
+// http://localhost:3000/products/1
+
+// 파일명에 대괄호를 쓰면 안에 있는 값은 변수처럼 쓸 수 있음 == 파람?
+//=> 주소가 바뀌어도 같은 페이지 보임 ==> 숫자가 들어간 자리가 id파람의 값임
